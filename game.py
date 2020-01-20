6a80963595cb3d561b3282dd718fe791697a4fed
kszczupak <kszczupak00@gmail.com>
2020-01-20 22:08:58 +0100

    Implemented basic Tetris logic, added preview window

--- game.py
+++ game.py
@@ -1,100 +1,274 @@
 import curses
-from time import sleep
+from time import time, sleep
 from curses.textpad import Textbox, rectangle
 
 from pieces import *
 
 
+PLAY_AREA_WIDTH = 10
+PLAY_AREA_HEIGHT = 20
+
+NEXT_PIECE_AREA_WIDTH = 4
+NEXT_PIECE_AREA_HEIGHT = 5
+
+
+def draw_piece(window, piece: AbstractPiece):
+		previous_positions = piece.previous_positions
+
+		if previous_positions:
+			for old_y, old_x in previous_positions:
+				window.addch(old_y, 2 * old_x + 1, " ")
+				window.addch(old_y, 2 * old_x + 2, " ")
+
+		for new_y, new_x in piece.current_positions:
+			# two curses.ACS_CKBOARD can be used also as one basic square
+			# first and last columns serve as borders, so +1 and +2 offsets needs to be used
+			# to account for that
+			window.addch(new_y, 2 * new_x + 1, "[")
+			window.addch(new_y, 2 * new_x + 2, "]")
+
+		window.refresh()
+
+
+def draw_next_piece(window, piece_class):
+	def erase_piece():
+		for y in range(2, NEXT_PIECE_AREA_HEIGHT):
+			for x in range(NEXT_PIECE_AREA_WIDTH):
+				window.addch(y, 2 * x + 1, " ")
+				window.addch(y, 2 * x + 2, " ")
+
+	erase_piece()
+
+	next_piece = piece_class(initial_rotation_block_position=(3, 2))
+
+	for y, x in next_piece.current_positions:
+		if isinstance(next_piece, LongBar) or isinstance(next_piece, Square):
+			# ugly way of adjusting Long Bar and Square to fit in the preview window
+			x_offset_1 = 1
+			x_offset_2 = 2
+		else:
+			x_offset_1 = 0
+			x_offset_2 = 1
+
+		window.addch(y, 2 * x + x_offset_1, "[")
+		window.addch(y, 2 * x + x_offset_2, "]")
+
+	window.refresh()
+
+
+def draw_stack(window, stack):
+	if stack['previous_positions']:
+		for y, x in stack['previous_positions']:
+			window.addch(y, 2 * x + 1, " ")
+			window.addch(y, 2 * x + 2, " ")
+
+		stack['previous_positions'] = None
+
+	for y, x in stack['positions']:
+		window.addch(y, 2 * x + 1, "[")
+		window.addch(y, 2 * x + 2, "]")
+
+	window.refresh()
+
+
+def validate_positions(requested_positions, stack):
+	for candidate_y, candidate_x in requested_positions:
+		if candidate_y <= 0 or candidate_y > PLAY_AREA_HEIGHT:
+			return False
+
+		if candidate_x < 0 or candidate_x >= PLAY_AREA_WIDTH:
+			return False
+
+		if (candidate_y, candidate_x) in stack['positions']:
+			return False
+
+	return True
+
+
+def is_inside_stack(requested_positions, stack):
+	for candidate_point in requested_positions:
+		if candidate_point in stack['positions']:
+			return True
+
+		candidate_y, _ = candidate_point
+
+		if candidate_y > PLAY_AREA_HEIGHT:
+			# last row
+			return True
+
+	return False
+
+
+def increase_stack(positions, stack):
+	affected_lines = set()
+
+	for position in positions:
+		stack['positions'].add(position)
+		line, _ = position
+		affected_lines.add(line)
+
+	return affected_lines
+
+
+def check_cleared_lines(stack, affected_lines):
+	cleared_lines = list()
+
+	for line in affected_lines:
+		line_cleared = True
+		for x in range(PLAY_AREA_WIDTH):
+			if (line, x) not in stack['positions']:
+				line_cleared = False
+				break
+
+		if line_cleared:
+			cleared_lines.append(line)
+
+	return cleared_lines
+
+
+def clear_lines(lines, stack):
+	def count_lines_above(line):
+		cnt = 0
+
+		for cleared_line in lines:
+			if line < cleared_line:
+				cnt += 1
+
+		return cnt
+
+	new_positions = set()
+
+	max_line = max(lines)
+	# update remaining stack positions
+	for position in stack['positions']:
+		y, x = position
+
+		if y in lines:
+			continue
+
+		if y > max_line:
+			# current position is below cleared lines
+			# position doesn't need to be updated
+			new_positions.add(position)
+		else:
+			new_position = (y + count_lines_above(y), x)
+			new_positions.add(new_position)
+
+	stack['previous_positions'] = stack['positions']
+	stack['positions'] = new_positions
+
+
+def clear_line_animation(window, lines):
+	def fill_with(char):
+		for line in lines:
+			for x in range(PLAY_AREA_WIDTH):
+				window.addch(line, 2 * x + 1, char)
+				window.addch(line, 2 * x + 2, char)
+
+		window.refresh()
+
+	def animation_cycle():
+		fill_with(curses.ACS_CKBOARD)
+		sleep(0.1)
+		fill_with(" ")
+		sleep(0.1)
+
+	if len(lines) == 4:
+		# tetris scored
+		nbr_of_cycles = 3
+	elif len(lines) == 3:
+		nbr_of_cycles = 2
+	else:
+		nbr_of_cycles = 1
+
+	for _ in range(nbr_of_cycles):
+		animation_cycle()
+
+
 def main(stdscr):
 	stdscr.keypad(True)
+	stdscr.nodelay(True)
 	height, width = stdscr.getmaxyx()
-
+	stdscr.refresh()
 	stdscr.addstr(1, width//2, "Command Line T.")
 
-	# editwin = curses.newwin(5,30, 2,1)
-	# rectangle(stdscr, 1,0, 1+5+1, 1+30+1)
-	rectangle(stdscr, 0, 0, 1, 1)
+	# no cursor
 	curses.curs_set(0)
 
-	# a = LongBar()
-	# a = L_Piece()
-	# a = J_Piece()
-	# a = Square()
-	# a = Z_Piece()
-	a = S_Piece()
-	a.debug_force_draw(stdscr)
+	# height and width of the new window + 2 to account for the borders
+	play_window = curses.newwin(PLAY_AREA_HEIGHT + 2,  2 * PLAY_AREA_WIDTH + 2, 5, width//2 - 10)
+	play_window.border()
 
-	for i in range(3):
-		a.advance()
-		a.debug_force_draw(stdscr)
+	# 5 (with text) x 4
+	next_piece_window = curses.newwin(5 + 2,  2 * 4 + 2, 5 + 8, width//2 + 12)
+	next_piece_window.border()
+	next_piece_window.addstr(1, 3, "NEXT")
+	next_piece_window.refresh()
 
-	a.rotate_clockwise()
-	a.debug_force_draw(stdscr)
+	block = get_random_piece()()
+	next_piece = get_random_piece()
 
-	for i in range(3, 6):
-		a.advance()
-		a.debug_force_draw(stdscr)
+	draw_next_piece(next_piece_window, next_piece)
 
-	a.rotate_clockwise()
-	a.debug_force_draw(stdscr)
+	draw_piece(play_window, block)
 
-	a.rotate_clockwise()
-	a.debug_force_draw(stdscr)
+	stack = {
+		"positions": set(),
+		"previous_positions": None
+	}
 
-	a.rotate_clockwise()	
-	a.debug_force_draw(stdscr)
+	timer = time()
+	time_interval = 1
 
-	a.rotate_anti_clockwise()
-	a.debug_force_draw(stdscr)
+	while True:
+		candidate_positions = None
+		advanced_positions = None
 
-	for i in range(6, 9):
-		a.advance()
-		a.debug_force_draw(stdscr)
+		c = stdscr.getch()
 
-	curses.beep()
-	# a = L_Piece()
-	# a.debug_force_draw(stdscr)
-	sleep(3)
+		if c == curses.KEY_RIGHT:
+			candidate_positions = block.move_right()
+		elif c == curses.KEY_LEFT:
+			candidate_positions = block.move_left()
+		elif c == curses.KEY_DOWN:
+			advanced_positions = block.advance()
+		elif c == ord('a'):
+			candidate_positions = block.rotate_clockwise()
+		elif c == ord('d'):
+			candidate_positions = block.rotate_anti_clockwise()
+		elif c == ord('q'):
+			break
+
+		if time() - timer >= time_interval:
+			advanced_positions = block.advance()
+			timer = time()
+
+		if candidate_positions:
+			if validate_positions(candidate_positions, stack):
+				block.accept_move()
+				draw_piece(play_window, block)
+			else:
+				block.reject_move()
+
+		if advanced_positions:
+			if is_inside_stack(advanced_positions, stack):
+				affected_lines = increase_stack(block.current_positions, stack)
+				cleared_lines = check_cleared_lines(stack, affected_lines)
+
+				if cleared_lines:
+					clear_line_animation(play_window, cleared_lines)
+					clear_lines(cleared_lines, stack)
+
+				block = next_piece()
+				next_piece = get_random_piece()
+				draw_next_piece(next_piece_window, next_piece)
+
+				draw_stack(play_window, stack)
+			else:
+				block.accept_move()
+
+			draw_piece(play_window, block)
 
 
 if __name__ == '__main__':
 	curses.wrapper(main)
-	# a = L_Piece()
-	# for i in range(3):
-	# 	print(f"Tick {i}:")
-	# 	print(a.blocks, end=" ")
-	# 	print(f"prev: {a.previous_positions}")
-	# 	print("-" * 10)
-	# 	a.advance()
-	# 	a.accept_move()
-
-	# print("Rotation: rotate_clockwise")
-	# a.rotate_clockwise()
-	# a.accept_move()
-	# print(a.blocks, end=" ")
-	# print(f"prev: {a.previous_positions}")
-	# print("-" * 10)
-
-	# for i in range(3, 6):
-	# 	print(f"Tick {i}:")
-	# 	print(a.blocks, end=" ")
-	# 	print(f"prev: {a.previous_positions}")
-	# 	print("-" * 10)
-	# 	a.advance()
-	# 	a.accept_move()
-
-	# print("Rotation: rotate_anti_clockwise")
-	# a.rotate_anti_clockwise()
-	# a.accept_move()
-	# print(a.blocks, end=" ")
-	# print(f"prev: {a.previous_positions}")
-	# print("-" * 10)
-
-	# for i in range(6, 9):
-	# 	print(f"Tick {i}:")
-	# 	print(a.blocks, end=" ")
-	# 	print(f"prev: {a.previous_positions}")
-	# 	print("-" * 10)
-	# 	a.advance()
-	# 	a.accept_move()
-
---
+++ pieces.py
@@ -0,0 +1,317 @@
+"""
+Collection of base pieces used in Tetris game.
+"""
+from enum import Enum
+import random
+
+
+def get_positions_from_rotation(rotation_block, orientation_enum):
+    positions = list()
+
+    for y_offset, x_offset in orientation_enum.value:
+        new_position = (
+            rotation_block[0] + y_offset,
+            rotation_block[1] + x_offset
+        )
+        positions.append(new_position)
+
+    return positions
+
+
+class AbstractPiece:
+    def __init__(self, initial_rotation_block_position=(1, 5)):
+        # Only position of rotation block will be constantly maintained - positions of other blocks can be
+        # calculated using rotation block and offsets relative to this block (encoded in orientation enum in this case)
+        self.rotation_block = initial_rotation_block_position
+        self.orientation = None
+
+        self.requested_rotation_block = None
+        self.requested_orientation = None
+
+        # Only for drawing - needs to be maintained in order to erase previous positions of the piece from window
+        self.previous_rotation_block = None
+        self.previous_orientation = None
+
+    def accept_move(self):
+        self.previous_rotation_block = self.rotation_block
+        if self.requested_rotation_block:
+            self.rotation_block = self.requested_rotation_block
+
+        self.previous_orientation = self.orientation
+        if self.requested_orientation:
+            self.orientation = self.requested_orientation
+
+    def reject_move(self):
+        self.requested_rotation_block = None
+        self.requested_orientation = None
+
+    @property
+    def previous_positions(self):
+        if self.previous_rotation_block:
+            return get_positions_from_rotation(self.previous_rotation_block, self.previous_orientation)
+
+        return None
+
+    @property
+    def current_positions(self):
+        return get_positions_from_rotation(self.rotation_block, self.orientation)
+
+    def move_right(self):
+        self.requested_rotation_block = (
+            self.rotation_block[0],
+            self.rotation_block[1] + 1
+        )
+
+        return get_positions_from_rotation(self.requested_rotation_block, self.orientation)
+
+    def move_left(self):
+        self.requested_rotation_block = (
+            self.rotation_block[0],
+            self.rotation_block[1] - 1
+        )
+
+        return get_positions_from_rotation(self.requested_rotation_block, self.orientation)
+
+    def advance(self):
+        self.requested_rotation_block = (
+            self.rotation_block[0] + 1,
+            self.rotation_block[1]
+        )
+
+        return get_positions_from_rotation(self.requested_rotation_block, self.orientation)
+
+    def rotate_clockwise(self):
+        raise NotImplementedError()
+
+    def rotate_anti_clockwise(self):
+        raise NotImplementedError()
+
+
+class Square(AbstractPiece):
+    """
+    0 1
+    2 3
+
+    Blocks numeration, rotation block = 1
+    """
+    class _Orientation(Enum):
+        CONSTANT = ((0, -1), (0, 0), (1, -1), (1, 0))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = Square._Orientation.CONSTANT
+
+    def rotate_clockwise(self):
+        # Square doesn't rotate so return current positions
+        return get_positions_from_rotation(self.rotation_block, self.orientation)
+
+    def rotate_anti_clockwise(self):
+        return self.rotate_clockwise()
+
+
+class LongBar(AbstractPiece):
+    """
+    0 1 2 3
+
+    Blocks numeration with orientation == _VERTICAL, rotation block = 2
+    """
+    class _Orientation(Enum):
+        VERTICAL = ((0, -2), (0, -1), (0, 0), (0, 1))
+        HORIZONTAL = ((2, 0), (1, 0), (0, 0), (-1, 0))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = LongBar._Orientation.VERTICAL
+
+    def rotate_clockwise(self):
+        if self.orientation == LongBar._Orientation.VERTICAL:
+            self.requested_orientation = LongBar._Orientation.HORIZONTAL
+        else:
+            self.requested_orientation = LongBar._Orientation.VERTICAL
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+    def rotate_anti_clockwise(self):
+        return self.rotate_clockwise()
+
+
+class L_Piece(AbstractPiece):
+    """
+    1 2 3
+    0
+
+    Blocks numeration when facing down (_Orientation.DOWN, rotation block = 2)
+    """
+
+    class _Orientation(Enum):
+        # abstract direction -> y, x offsets relative to rotation block
+        DOWN = ((1, -1), (0, -1), (0, 0), (0, 1))
+        LEFT = ((-1, -1), (-1, 0), (0, 0), (1, 0))
+        UP = ((-1, 1), (0, 1), (0, 0), (0, -1))
+        RIGHT = ((1, 1), (1, 0), (0, 0), (-1, 0))
+
+    _clockwise_rotation = {
+        _Orientation.DOWN: _Orientation.LEFT,
+        _Orientation.LEFT: _Orientation.UP,
+        _Orientation.UP: _Orientation.RIGHT,
+        _Orientation.RIGHT: _Orientation.DOWN,
+    }
+
+    _anti_clockwise_rotation = {v: k for k, v in _clockwise_rotation.items()}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = L_Piece._Orientation.DOWN
+
+    def rotate_clockwise(self):
+        self.requested_orientation = L_Piece._clockwise_rotation[self.orientation]
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+    def rotate_anti_clockwise(self):
+        self.requested_orientation = L_Piece._anti_clockwise_rotation[self.orientation]
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+
+class J_Piece(AbstractPiece):
+    """
+    0 1 2
+        3
+
+    Blocks numeration when facing down (_Orientation.DOWN, rotation block = 1)
+    """
+
+    class _Orientation(Enum):
+        # abstract direction -> y, x offsets relative to rotation block
+        DOWN = ((0, -1), (0, 0), (0, 1), (1, 1))
+        LEFT = ((-1, 0), (0, 0), (1, 0), (1, -1))
+        UP = ((0, 1), (0, 0), (0, -1), (-1, -1))
+        RIGHT = ((1, 0), (0, 0), (-1, 0), (-1, 1))
+
+    _clockwise_rotation = {
+        _Orientation.DOWN: _Orientation.LEFT,
+        _Orientation.LEFT: _Orientation.UP,
+        _Orientation.UP: _Orientation.RIGHT,
+        _Orientation.RIGHT: _Orientation.DOWN,
+    }
+
+    _anti_clockwise_rotation = {v: k for k, v in _clockwise_rotation.items()}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = J_Piece._Orientation.DOWN
+
+    def rotate_clockwise(self):
+        self.requested_orientation = J_Piece._clockwise_rotation[self.orientation]
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+    def rotate_anti_clockwise(self):
+        self.requested_orientation = J_Piece._anti_clockwise_rotation[self.orientation]
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+
+class Z_Piece(AbstractPiece):
+    """
+    0 1
+      2 3
+
+    Blocks numeration when facing left (_Orientation.LEFT, rotation block = 1)
+    """
+
+    class _Orientation(Enum):
+        # abstract direction -> y, x offsets relative to rotation block
+        LEFT = ((0, -1), (0, 0), (1, 0), (1, 1))
+        UP = ((-1, 0), (0, 0), (0, -1), (1, -1))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = Z_Piece._Orientation.LEFT
+
+    def rotate_clockwise(self):
+        if self.orientation == Z_Piece._Orientation.UP:
+            self.requested_orientation = Z_Piece._Orientation.LEFT
+        else:
+            self.requested_orientation = Z_Piece._Orientation.UP
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+    def rotate_anti_clockwise(self):
+        return self.rotate_clockwise()
+
+
+class S_Piece(AbstractPiece):
+    """
+      0 1
+    2 3
+
+    Blocks numeration when facing right (_Orientation.RIGHT, rotation block = 0)
+    """
+
+    class _Orientation(Enum):
+        # abstract direction -> y, x offsets relative to rotation block
+        RIGHT = ((0, 0), (0, 1), (1, -1), (1, 0))
+        UP = ((0, 0), (-1, 0), (0, 1), (1, 1))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = S_Piece._Orientation.RIGHT
+
+    def rotate_clockwise(self):
+        if self.orientation == S_Piece._Orientation.UP:
+            self.requested_orientation = S_Piece._Orientation.RIGHT
+        else:
+            self.requested_orientation = S_Piece._Orientation.UP
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+    def rotate_anti_clockwise(self):
+        return self.rotate_clockwise()
+
+
+class T_Piece(AbstractPiece):
+    """
+    0 1 2
+      3
+
+    Blocks numeration when facing down (_Orientation.DOWN, rotation block = 1)
+    """
+
+    class _Orientation(Enum):
+        # abstract direction -> y, x offsets relative to rotation block
+        DOWN = ((0, -1), (0, 0), (0, 1), (1, 0))
+        LEFT = ((-1, 0), (0, 0), (1, 0), (0, -1))
+        UP = ((0, 1), (0, 0), (0, -1), (-1, 0))
+        RIGHT = ((1, 0), (0, 0), (-1, 0), (0, 1))
+
+    _clockwise_rotation = {
+        _Orientation.DOWN: _Orientation.LEFT,
+        _Orientation.LEFT: _Orientation.UP,
+        _Orientation.UP: _Orientation.RIGHT,
+        _Orientation.RIGHT: _Orientation.DOWN,
+    }
+
+    _anti_clockwise_rotation = {v: k for k, v in _clockwise_rotation.items()}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = T_Piece._Orientation.DOWN
+
+    def rotate_clockwise(self):
+        self.requested_orientation = T_Piece._clockwise_rotation[self.orientation]
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+    def rotate_anti_clockwise(self):
+        self.requested_orientation = T_Piece._anti_clockwise_rotation[self.orientation]
+
+        return get_positions_from_rotation(self.rotation_block, self.requested_orientation)
+
+
+all_pieces = (Square, LongBar, L_Piece, J_Piece, Z_Piece, S_Piece, T_Piece)
+
+
+def get_random_piece():
+    return random.choice(all_pieces)
